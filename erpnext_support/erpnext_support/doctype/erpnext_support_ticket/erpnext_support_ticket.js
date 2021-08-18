67aaffe9557bc2509daa6d22653c8ed1700776a1
lasalesi <larsericmueller@gmail.com>
2021-08-18 23:18:15 +0200

    update

---
+++ erpnext_support/erpnext_support/doctype/erpnext_support_ticket/erpnext_support_ticket.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, anonymous and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ERPNext Support Ticket', {
+	// refresh: function(frm) {
+
+	// }
+});
